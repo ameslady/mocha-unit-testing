10a5137ed8e41798fb2eda3e77df2208b9ada7eb
Amy Martin <amymariemart@gmail.com>
2022-04-10 17:53:59 -0700

    Ensured all mocha unit tests pass

--- test/arrayTest.js
+++ test/arrayTest.js
@@ -1,11 +1,11 @@
 //  Uncomment if using Node
-// var chai = require('chai');
+ var chai = require('chai');
 var assert = chai.assert;
 
 describe('Array', function() {
   it('should start empty', function() {
     var arr = [];
 
-    assert.equal(arr.length, 0);
+    assert.equal(arr.length, 0, "Array length is not zero");
   });
 });
